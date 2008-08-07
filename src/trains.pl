4d167b815fa526670be698f1a7123ddaa245e25d
Joseph Jon Booker <joe@neoturbine.net>
2008-08-07 17:18:24 -0500

    Added copyright notices and GPLv3+

--- src/trains.pl
+++ src/trains.pl
@@ -1,4 +1,22 @@
 #!/usr/bin/perl
+=pod
+/********************************************************************
+* This file is part of ChiTransit.
+
+* ChiTransit is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* ChiTransit is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with ChiTransit.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+=cut
 
 # we are generating c code to make gtk objects
 # perl handles multidiminsional arrays better
